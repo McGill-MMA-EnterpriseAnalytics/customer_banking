f4b80cd35c6eb9cd80593c49e51453af1fa0bf00
Carlos Adrián Alarcón <83436724+aladelca@users.noreply.github.com>
2024-04-26 10:50:42 -0400

    Update predict.py

--- 09_model_pipeline/predict/src/predict.py
+++ 09_model_pipeline/predict/src/predict.py
@@ -3,8 +3,8 @@ import numpy
 import pickle
 class Predict:
     def main_predict(data, bucket_name, path):
-        #model = load_models_from_s3(bucket_name,  path )
-        model = pickle.load(open("09_model_pipeline/predict/src/trained_model.pickle",'rb'))
+        model = load_models_from_s3(bucket_name,  path )
+        #model = pickle.load(open("09_model_pipeline/predict/src/trained_model.pickle",'rb'))
         probas = model.predict_proba(data)
         preds = np.where(probas[:,1] >= 0.4, 1, 0)
         data['prob_0'] = probas[:,0]
